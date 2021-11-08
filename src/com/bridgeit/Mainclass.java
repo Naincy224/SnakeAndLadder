48531a3388cbbc453f94dc0bf97a25f6e1963cc6
Naincy224 <pihusingh224@gmail.com>
2021-11-08 17:21:56 +0530

    Adding UC1 for snake and ladder program

---
+++ src/com/bridgeit/Mainclass.java
@@ -0,0 +1,14 @@
+package com.bridgeit;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		
+
+		SnakeAndLadder snakeladder = new SnakeAndLadder();
+		snakeladder.position();
+		int diceRoll = snakeladder.diceRoll();
+		
+	}
+
+}
---
+++ src/com/bridgeit/SnakeAndLadder.java
@@ -0,0 +1,26 @@
+package com.bridgeit;
+
+public class SnakeAndLadder {
+	
+	
+//uc1	
+	public void position() {
+		int position=0;
+		System.out.println("Position : " + position);
+		
+	}
+	
+//uc2
+	public int diceRoll() {
+		
+		int diceRoll = (int)Math.floor ((Math.random() * 10 % 6) + 1);
+		return diceRoll;
+		
+		
+	}
+	
+//uc3
+	
+	
+
+}
