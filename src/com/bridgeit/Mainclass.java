bd0acbc1fc32d08c4f755505919ebbb909dbb8c7
Naincy224 <pihusingh224@gmail.com>
2021-11-09 08:39:28 +0530

    Adding uc6 for SnakeAndLadder

--- src/com/bridgeit/Mainclass.java
+++ src/com/bridgeit/Mainclass.java
@@ -7,12 +7,16 @@ public class Mainclass {
 
 		SnakeAndLadder snakeladder = new SnakeAndLadder();
 		int position= snakeladder.getPosition();
+		
+		while (position < 100) {
 		System.out.println("Position of the player : " +position);
 		int diceRoll = snakeladder.diceRoll();
 		System.out.println("Number after rolling dice is : " +diceRoll);
 		int option = snakeladder.generateOption();
 		System.out.println("Option is : " + option);
 		snakeladder.switchPlayoption(option, diceRoll);
+		position = snakeladder.getPosition();
+		}
 
 	}
 
